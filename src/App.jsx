927614c0e8384b42323b65b747b1a0403270f064
kolanpaka kalyan <kolanpaka2000@gmail.com>
2025-09-04 10:27:34 +0530

    optimize App.jsx imports with React.lazy and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AppLayout from "./AppLayout";
-import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Login from "./pages/Login";
-import Account from "./pages/Account";
+import { Suspense, lazy, useState } from "react";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
-import ViewDetails from "./features/bookings/ViewDetails";
-import Checkin from "./pages/Checkin";
+import Spinner from "./UI/Spinner";
+import AppLayout from "./AppLayout";
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+const Cabins = lazy(() => import("./pages/Cabins"));
+const Users = lazy(() => import("./pages/Users"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Login = lazy(() => import("./pages/Login"));
+const Account = lazy(() => import("./pages/Account"));
+const ViewDetails = lazy(() => import("./features/bookings/ViewDetails"));
+const Checkin = lazy(() => import("./pages/Checkin"));
 
 export default function App() {
   const [isSessionExpired, setSessionExpired] = useState("initial");
@@ -18,34 +19,36 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route
-            path="login"
-            element={
-              <Login
-                isSessionExpired={isSessionExpired}
-                setSessionExpired={setSessionExpired}
-              />
-            }
-          />
-          <Route
-            path="/"
-            element={<AppLayout setSessionExpired={setSessionExpired} />}
-          >
+        <Suspense fallback={<Spinner />}>
+          <Routes>
             <Route
-              index
-              element={<Navigate to="dashboard?duration=7" replace />}
+              path="login"
+              element={
+                <Login
+                  isSessionExpired={isSessionExpired}
+                  setSessionExpired={setSessionExpired}
+                />
+              }
             />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="bookings/:bookingId" element={<ViewDetails />} />
-            <Route path="checkin/:bookingId" element={<Checkin />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Account />} />
-          </Route>
-        </Routes>
+            <Route
+              path="/"
+              element={<AppLayout setSessionExpired={setSessionExpired} />}
+            >
+              <Route
+                index
+                element={<Navigate to="dashboard?duration=7" replace />}
+              />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="bookings/:bookingId" element={<ViewDetails />} />
+              <Route path="checkin/:bookingId" element={<Checkin />} />
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="account" element={<Account />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <Toaster />
     </>
